2b0ff5c9452c5b9064d296e5b4a9cb14e4b6e7fb
Shailiza Manandhar <shailzam@msn.com>
2016-02-08 19:56:47 -0600

    Rocket launched

--- js/rocket.js
+++ js/rocket.js
@@ -1,5 +1,6 @@
 var timer = null;
-countdownNumber = 10;
+var countdownNumber = 10;
+
 var changeState = function (state) {
 	document.body.className = 'body-state' + state;
 	clearInterval(timer);
@@ -11,21 +12,34 @@ var changeState = function (state) {
 		timer = setInterval(function () {
 			countdownNumber = countdownNumber -1;
 			document.getElementById('countdown').innerHTML = countdownNumber;
+			
+			if (countdownNumber > 4 && countdownNumber <= 7) {
+				//be nervous
+				document.getElementById('nervous').className = 'nervous show';
+			} else {
+				document.getElementById('nervous').className = 'nervous';
+			}
+
+			if (countdownNumber > 1 && countdownNumber <= 4) {
+				//cant-wait
+				document.getElementById('cant-wait').className = 'cant-wait show';
+			} else {
+				document.getElementById('cant-wait').className = 'cant-wait';
+			}
+
 			if (countdownNumber <= 0) {
-				
 				changeState(3);
 			}
 
 		}, 500);
 	} else if (state == 3) {
-		var success = setTimeout(function ()
-		{
+		var success = setTimeout(function () {
 			var randomNumber = Math.round(Math.random()*10);
 
 			console.log('randomNumber: ', randomNumber);
 
 			//success
-			if (randomNumber > 5) {
+			if (randomNumber > 2) {
 				changeState(4);
 			} else {
 				changeState(5); //oh no!
